015ec4f92429c24e76e7cf30aaa94dd174db3bfc
Jianzhu Zhang <jianzzha@redhat.com>
2020-05-26 12:19:46 -0400

    update size in uperf cmd.sh

--- uperf/cmd.sh
+++ uperf/cmd.sh
@@ -4,7 +4,7 @@
 #	mode (default manual)
 # additional env for master:
 #       uperfSlave (slave address)
-#       writeSize (write size to slave, default 8192)
+#       size (write size to slave, default 8192)
 #       readSize (read size from slave, default 8192)
 #       duration (default 66s)
 
@@ -34,21 +34,20 @@ elif [[ "${mode}" == "master" ]]; then
 		echo "env var: uperfSlave needs to be defined for uperf master"
 	else
 		export uperfSlave=${uperfSlave}
-		export writeSize=${writeSize:-1024}
-		export readSize=${readSize:-1024}
+		export size=${size:-1024}
 		export duration=${duration:-60s}
 		export testType=rr
 		export threads=${threads:-1}
 		cat <<EOF >request-response.xml
 <?xml version="1.0"?>
-<profile name="tcp-${testType}-${writeSize}B-${threads}i">
+<profile name="tcp-${testType}-${size}B-${threads}i">
   <group nthreads="${threads}">
     <transaction iterations="1">
       <flowop type="connect" options="remotehost=${uperfSlave} protocol=tcp"/>
     </transaction>
     <transaction duration="${duration}">
-      <flowop type="write" options="size=${writeSize}"/>
-      <flowop type="read"  options="size=${readSize}"/>
+      <flowop type="write" options="size=${size}"/>
+      <flowop type="read"  options="size=${size}"/>
     <transaction iterations="1">
       <flowop type="disconnect" />
     </transaction>
